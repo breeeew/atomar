79d9be25f1e87aa31cc01426ca26740ccab9d7f3
a.bayramov <a.bayramly@corp.mail.ru>
2022-05-12 11:14:01 +0300

    v0.0.7

--- src/lib/react/types.ts
+++ src/lib/react/types.ts
@@ -1,8 +1,9 @@
-import {Observable} from "rxjs"
-import {ReactElement} from "react";
+import type {Observable} from "rxjs"
+import type {ReactElement} from "react";
+import type {Wrapped} from "../wrapped/types";
 
 export type RxProps<T> = {
-    value$: Observable<T>
+    value$: Observable<T | Wrapped<T>>
     children(props: T): ReactElement
     rejected?(error: Error, reload?: () => void): ReactElement
     pending?(): ReactElement
