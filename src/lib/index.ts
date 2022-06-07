255450fcdf925ce7dcd4a82576e85beada1704d3
a.bayramov <a.bayramly@corp.mail.ru>
2022-06-07 08:04:41 +0300

    v0.0.10

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,10 +3,11 @@ import {Lens} from "./lens"
 import {FormStore} from "./form/FormStore"
 import {useRx} from "./react/useRx"
 import {Rx} from "./react/Rx"
+import {RxIf} from "./react/RxIf";
 import {createFulfilledWrapped, createRejectedWrapped, wrap, isWrapped, pendingWrapped, idleWrapped} from "./wrapped/base";
 import {validateJoi} from "./form/validation";
 
-export {Atom, Lens, FormStore, Rx, useRx}
+export {Atom, Lens, FormStore, Rx, RxIf, useRx}
 export {createFulfilledWrapped, createRejectedWrapped, wrap, isWrapped, pendingWrapped, idleWrapped}
 export {validateJoi}
 export * from "./form/types"
---
+++ src/lib/react/RxIf.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { Observable } from "rxjs"
+import { useRx } from "./useRx"
+type OrReactChild<T> = React.ReactChild | React.ReactChild[] | T
+
+export interface RxIfProps {
+    test$: Observable<any>
+    else?: OrReactChild<() => React.ReactNode>
+    negate?: boolean
+    children: React.ReactNode
+}
+
+export function RxIf({ test$, children, negate, else: not }: RxIfProps): React.ReactElement | null {
+    const raw = useRx(test$)
+    const truthy = raw.status === "fulfilled" && Boolean(raw.value)
+
+    if (negate && !truthy) {
+        return <>{children}</>
+    } else if (negate) {
+        if (typeof not === "function") return <>{not()}</>
+        else return <>{not}</>
+    } else if (truthy) {
+        return <>{children}</>
+    } else {
+        if (typeof not === "function") return <>{not()}</>
+        else return <>{not}</>
+    }
+}
