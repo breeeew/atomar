e237fd6140832d68fae0e801502f154821b6a403
a.bayramov <a.bayramly@corp.mail.ru>
2022-08-31 09:41:56 +0300

    react: change children type, fix type errors

--- packages/react/src/Rx.tsx
+++ packages/react/src/Rx.tsx
@@ -5,22 +5,18 @@ export function Rx<T>(props: RxProps<T>) {
     const data = useRx(props.value$)
 
     if (data.status === 'pending') {
-        if (props.pending) {
-            return props.pending()
-        }
+        if (props.pending) return <>{props.pending()}</>
         return null
     }
 
     if (data.status === 'rejected') {
-        return props.rejected?.(data.error, data.reload) ?? <>{data.error.message}</>
+        return <>{props.rejected?.(data.error, data.reload) ?? data.error.message}</>
     }
 
     if (data.status === 'idle') {
-        if (props.idle) {
-            return props.idle()
-        }
+        if (props.idle) return <>{props.idle()}</>
         return null
     }
 
-    return props.children(data.value)
+    return <>{props.children(data.value)}</>
 }
