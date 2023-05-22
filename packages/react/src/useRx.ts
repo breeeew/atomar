b63ad0b0aadcd81ab7ecb3f8d526b078863d1655
a.bayramov <a.bayramly@corp.mail.ru>
2023-05-22 09:02:49 +0300

    fix: useRx and Rx weren't update value if incoming observable changed

--- packages/react/src/useRx.ts
+++ packages/react/src/useRx.ts
@@ -1,31 +1,13 @@
-import {useEffect, useMemo, useRef, useState} from "react"
+import {useEffect, useMemo, useState} from "react"
 import {pendingWrapped, wrap} from "@atomrx/wrapped";
 import {Wrapped, WrappedObservable} from "@atomrx/wrapped";
 
-export function useRx<T>(observable: WrappedObservable<T>, deps: any[] = [observable]): Wrapped<T> {
-    const [, setCount] = useState<number>(0)
-    const value = useRef<Wrapped<T>>(pendingWrapped)
-    const initial = useRef(true)
-    const memoized = useMemo(() => wrap(observable), [observable])
-
-    const sub = useMemo(
-        () =>
-            memoized.subscribe(next => {
-                const current = value.current
-                value.current = next
-                if (!initial.current) {
-                    if (
-                        current.status !== next.status ||
-                        (current.status === "fulfilled" && next.status === "fulfilled" && current.value !== next.value)
-                    ) {
-                        setCount(c => c + 1)
-                    }
-                }
-            }),
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        deps
-    )
-    useEffect(() => () => sub.unsubscribe(), [sub])
-    initial.current = false
-    return value.current
+export function useRx<T>(observable: WrappedObservable<T>): Wrapped<T> {
+    const [value, setValue] = useState<Wrapped<T>>(pendingWrapped);
+    const memoized = useMemo(() => wrap(observable), [observable]);
+    useEffect(() => {
+        const sub = memoized.subscribe(setValue);
+        return () => sub.unsubscribe();
+    }, [memoized]);
+    return value;
 }
